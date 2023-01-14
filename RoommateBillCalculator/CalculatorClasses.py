375bddb1c37d8ebbc955870cd4d509038b47d221
wcscherer <wscherer25@gmail.com>
2023-01-14 15:23:54 -0500

    renamed file containing classes and moved supporting files to /files dir

---
+++ RoommateBillCalculator/CalculatorClasses.py
@@ -0,0 +1,166 @@
+from fpdf import FPDF
+import webbrowser
+import os
+
+class Bill(object):
+    """
+    Class defining the amount to be paid (bill) by both roommates over the given rental period (month, year)
+
+    Paramters
+    ----------
+    amount: float - total amount of money owed by the roommates for the given pay period
+
+    period: str - period the amount is to be paid for (month, year)
+
+    Methods
+    ----------
+
+    """
+
+    def __init__(self, amount, period):
+        if type(amount) not in [int, float]:
+            raise TypeError('Amount must be int or float')
+        if type(period) not in [str]:
+            raise TypeError('period must be input as a string')
+        self.amount = amount
+        self.period = period
+
+
+class Roommate(object):
+    """
+    Class defining the roommate who lives in the apartment for the rental period.
+
+    Parameters
+    -----------
+    name : str - name of roommate in apartment
+
+    days_in_unit : int - number of whole days the given roommate was in the unit
+
+    Methods
+    -----------
+    pay_bill : arg: bill object - method of calculating how much the roommates have to pay based upon the bill (Bill instance)
+    """
+
+    def __init__(self, name, days_in_unit):
+        if type(days_in_unit) not in [int,]:
+            raise TypeError('days_in_unit must be int or float')
+        if type(name) not in [str]:
+            raise TypeError('name must be input as a string')
+        self.name = name
+        self.days_in_unit = days_in_unit
+
+    def pay_bill(self, bill_instance, other_roommate) -> float:
+        """
+        Assigns a bill to be paid by the roommate
+
+        Parameters
+        -----------
+        bill_instance: Bill obj - the bill to be split and paid by roommates for given pay period and fractional occupancy
+
+        other_roommate: Roommate obj - instance of other roommate sharing the apartment
+
+        Returns
+        -----------
+        bill_amount: float - fractional bill to be paid by roommate instance based upon number of days in unit and number of days
+        other roommate occupied house
+
+        """
+        # bill amount per user depends on their fractional time spent in the unit and the total occupancy for that pay period 
+        bill_fraction = self.days_in_unit / (self.days_in_unit + other_roommate.days_in_unit)
+
+        bill_amount = bill_fraction * bill_instance.amount
+
+        return bill_amount
+
+class PdfReport(object):
+    """
+    Class defining the pdf report that will be outputted to the selected directory containing the amount owed by each roommate
+    for the given rental pay period.
+
+    Parameters
+    -----------
+    file_name: str - user defined name for the generated pdf report
+
+    rommate_name: str - name of the roommate also staying in the unit
+
+    rental_period: tuple (str, int) - rental period for the bill to be paid (month, year)
+
+    bill: Bill obj - the bill object for the bill to be paid for the given rental period
+
+    Methods
+    -----------
+    generate_pdf: - creates the pdf containing the fraction of the bill to be paid by each roommate for the given period
+
+    """
+
+    def __init__(self, file_name) -> None:
+        if type(file_name) not in [str]:
+            raise TypeError('file_name must be input as a string')
+        self.file_name = file_name
+    
+    def generate_pdf(self, roommate1, roommate2, bill):
+        """
+        Generates the pdf report containing the fraction of the bill to be paid by each roommate for the given rental period.
+
+        Parameters
+        -----------
+        roommate1: Roommate obj - person 1 living in the rental unit for the given rental period
+
+        roommate2: Roommate obj - person 2 living in the rental unit for the given rental period
+
+        bill: Bill obj - the bill instance to be paid by the two roommates living in the unit for the given period
+
+        Returns
+        -----------
+        pdf_bill: pdf file - file containing the fraction of the bill to be paid by each roommate for the given period
+        """
+        # Generate the pdf object and add a page to edit
+        pdf_bill = FPDF(orientation='P', unit='pt', format='A4')
+        pdf_bill.add_page()
+
+        # Add house image to header
+        pdf_bill.image("./files/house.png", x= 60, y=52, w=30, h=30)
+        pdf_bill.image("./files/house.png", x= 507, y=52, w=30, h=30)
+
+
+        # Add desired bold title text and image to the pdf based upon user input
+        pdf_bill.set_font(family='Times', size=24, style='B')
+        pdf_bill.cell(w=0, h=80, txt="Roommate Utility Bill", border=1, align='C', ln=1)
+
+        # Add text box for the rental period
+        pdf_bill.cell(w=160, h=40, txt="Rental Period:", border=0)
+        pdf_bill.cell(w=150, h=40, txt=str(bill.period), border=0, ln=1)
+
+        # Add text box for total monthly bill
+        pdf_bill.set_font(family='Times', size=18, style='B')
+        pdf_bill.cell(w=110, h=40, txt="Total Bill ($):", border=0)
+        pdf_bill.cell(w=40, h=40, txt="{:.2f}".format(bill.amount), align='L', border=0, ln=1)
+        pdf_bill.cell(w=250, h=40, txt="Cumulative Occupancy (Days):", border=0)
+        pdf_bill.cell(w=40, h=40, txt=str(roommate1.days_in_unit + roommate2.days_in_unit), align='L', border=0, ln=1)
+
+        # Text block headers for amount roommates have to pay
+        pdf_bill.set_font(family='Times', size=18, style='I')
+        pdf_bill.cell(w=120, h=40, txt="Roommate ", align='C', border=0)
+        pdf_bill.cell(w=160, h=40, txt="Days Occupied ", align='C', border=0)
+        pdf_bill.cell(w=120, h=40, txt="Owes ($) ", align='C', border=0, ln=1)
+
+        # Text block for amount roommate 1 has to pay
+        pdf_bill.set_font(family='Times', size=18, style='')
+        pdf_bill.cell(w=120, h=40, txt=str(roommate1.name), align='C', border=0)
+        pdf_bill.cell(w=160, h=40, txt=str(roommate1.days_in_unit), align='C', border=0)
+        pdf_bill.cell(w=110, h=40, txt="{:.2f}".format(roommate1.pay_bill(bill, roommate2)), align='C', border=0, ln=1)
+
+        # Text block for amount roommate 2 has to pay
+        pdf_bill.cell(w=120, h=40, txt=str(roommate2.name), align='C', border=0)
+        pdf_bill.cell(w=160, h=40, txt=str(roommate2.days_in_unit), align='C', border=0)
+        pdf_bill.cell(w=110, h=40, txt="{:.2f}".format(roommate2.pay_bill(bill, roommate1)), align='C', border=0, ln=1)
+
+        # Save file to current directory
+        if ".pdf" not in str(self.file_name):
+            self.file_name = str(self.file_name)+".pdf"
+        else:
+            pass
+
+        # save the pdf file and open in a web browser
+        pdf_bill.output(self.file_name)
+        webbrowser.open('file://'+os.path.realpath(self.file_name))
--- RoommateBillCalculator/RoommateBillCalculator.py
+++ RoommateBillCalculator/RoommateBillCalculator.py
@@ -1,4 +1,4 @@
-import main
+from CalculatorClasses import Bill, Roommate, PdfReport
 ## CLI IMPLEMENTATION
 
 # Welcome banner
@@ -8,18 +8,18 @@ print("\n*** WELCOME TO THE ROOMMATE BILL CALCULATOR ***\n")
 bill_period = input("Enter the bill period as MONTH YEAR (e.g. May 2022): ")
 
 bill_amount = float(input("Enter the bill amount in dollars and cents (e.g. 1503.12): "))
-bill_instance = main.Bill(amount=bill_amount, period=bill_period)
+bill_instance = Bill(amount=bill_amount, period=bill_period)
 print("Billing period: {}; Bill amount: {:.2f}\n".format(bill_period, bill_amount))
 
 # Input the user name and occupancy
 user_name = str(input("Enter your full name (e.g. John Doe): "))
 user_occupancy = int(input("Enter the number of days you, {}, resided in the apartment during the pay period: ".format(user_name)))
-user = main.Roommate(name=user_name, days_in_unit=user_occupancy)
+user = Roommate(name=user_name, days_in_unit=user_occupancy)
 
 # Input the roommate's name and occupancy
 roommate_name = str(input("Please enter your roommate's full name (e.g. Jane Doe): "))
 roommate_occupancy = int(input("Enter the number of days your roommate, {}, resided in the apartment during the pay period: ".format(roommate_name)))
-user_roommate = main.Roommate(name=roommate_name, days_in_unit=roommate_occupancy)
+user_roommate = Roommate(name=roommate_name, days_in_unit=roommate_occupancy)
 
 # Calculate the fraction of the bill to be paid and print the pdf
 user_pays = user.pay_bill(bill_instance=bill_instance, other_roommate=user_roommate)
@@ -27,5 +27,5 @@ roommate_pays = user_roommate.pay_bill(bill_instance=bill_instance, other_roomma
 print("Roommate {} pays: {:.2f}".format(user.name, user_pays))
 print("Roommate {} pays: {:.2f}".format(user_roommate.name, roommate_pays))
 
-bill_report = main.PdfReport(file_name="Roommate-Bill-for-"+str(bill_instance.period).replace(" ","-")+'.pdf')
+bill_report = PdfReport(file_name="Roommate-Bill-for-"+str(bill_instance.period).replace(" ","-")+'.pdf')
 bill_report.generate_pdf(roommate1=user, roommate2=user_roommate, bill=bill_instance)
